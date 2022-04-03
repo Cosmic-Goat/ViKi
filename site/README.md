cdf4e17e1a96ed442721dd687dc0f9cd056c25a6
Shrey Pasricha <shrey.pasricha@gmail.com>
2022-04-03 12:57:47 -0400

    Inital commit of static site.

---
+++ site/README.md
@@ -0,0 +1,6 @@
+# Tiny Embedded Data Viewer 
+
+This is a very small static site to be bundled with our Skunkworks hackathon medical device project.
+
+> ✨ Bootstrapped with Create Snowpack App (CSA).
+
---
+++ site/src/index.ts
@@ -0,0 +1,41 @@
+import { CategoryScale, Chart, LinearScale, LineController, LineElement, PointElement } from "chart.js";
+
+Chart.register(LineElement, LineController, CategoryScale, LinearScale, PointElement);
+
+const getData = async (file: string) => new Uint32Array(await (await (await fetch(file)).blob()).arrayBuffer());
+
+const bpmData = await getData("bpm.dat")
+const tempData = getData("temp.dat");
+const oxData = getData("ox.dat");
+
+const config = {
+  labels: [1, 2, 3, 4, 5, 6, 7],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: bpmData,
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
+
+
+const bpmChart = new Chart("bpm", {
+  type: "line",
+  data: config,
+  options: {}
+});
+
+const tempChart = new Chart("temp", {
+  type: "line",
+  data: config,
+  options: {}
+});
+
+const oxChart = new Chart("ox", {
+  type: "line",
+  data: config,
+  options: {}
+});
