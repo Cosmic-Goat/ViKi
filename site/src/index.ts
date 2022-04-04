41737897d30e975b497765e31eee1908e59bca6d
Shrey Pasricha <shrey.pasricha@gmail.com>
2022-04-04 15:15:47 -0400

    Responsive site charts that fill the page.

--- site/src/index.ts
+++ site/src/index.ts
@@ -1,10 +1,13 @@
 import {
   CategoryScale,
   Chart,
+  ChartData,
   LinearScale,
   LineController,
   LineElement,
   PointElement,
+  Title,
+  Tooltip
 } from 'chart.js';
 
 Chart.register(
@@ -13,6 +16,8 @@ Chart.register(
   CategoryScale,
   LinearScale,
   PointElement,
+  Title,
+  Tooltip
 );
 
 async function main() {
@@ -23,12 +28,12 @@ async function main() {
   // const tempData = await getData('./temp.dat');
   // const oxData = await getData('./ox.dat');
 
-  const config = {
+  const config: ChartData = {
     labels: [1, 2, 3, 4, 5, 6, 7],
     datasets: [
       {
         label: 'My First Dataset',
-        data: bpmData,
+        data: Array.from(bpmData),
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
@@ -36,23 +41,20 @@ async function main() {
     ],
   };
 
-  const bpmChart = new Chart('bpm', {
-    type: 'line',
-    data: config,
-    options: {},
-  });
+  const makeChart = (id: string, title: string, data: ChartData) =>
+    new Chart(id, {
+      type: 'line',
+      data: data,
+      options: {
+        maintainAspectRatio: false,
+        plugins: { title: { display: true, text: title } },
+      },
+    });
 
-  const tempChart = new Chart('temp', {
-    type: 'line',
-    data: config,
-    options: {},
-  });
-
-  const oxChart = new Chart('ox', {
-    type: 'line',
-    data: config,
-    options: {},
-  });
+  const pulseChart = makeChart('pulse', 'Pulse', config);
+  const bpmChart = makeChart('bpm', 'Heart Rate', config);
+  const tempChart = makeChart('temp', 'Body Temperature', config);
+  const o2Chart = makeChart('ox', 'Blood Oxygen Concentration', config);
 }
 
 window.addEventListener('load', main);
